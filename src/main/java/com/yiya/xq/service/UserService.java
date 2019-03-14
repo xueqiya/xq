21b3a1fa534f020d53d7377104a4bc067c1d4285
xueqiya <jiaowoxueqiya@gmail.com>
2019-03-14 17:10:29 +0800

    完成删除

--- src/main/java/com/yiya/xq/service/UserService.java
+++ src/main/java/com/yiya/xq/service/UserService.java
@@ -4,12 +4,10 @@ import com.yiya.xq.bean.ResultBean;
 import com.yiya.xq.bean.UserBean;
 import com.yiya.xq.repository.UserRepository;
 import com.yiya.xq.utils.ResultUtil;
-import org.hibernate.validator.constraints.EAN;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Service
@@ -19,29 +17,32 @@ public class UserService {
     private UserRepository userRepository;
 
     public ResultBean register(UserBean userBean) {
-        if (userBean.getUid() == null && userBean.getPwd() == null) {
+        UserBean bean = userRepository.findUserBeanByUid(userBean.getUid());
+        if (bean != null) {
+            return ResultUtil.error(601, "用户已注册");
+        } else if (userBean.getUid() == null && userBean.getPwd() == null) {
             return ResultUtil.error(601, "账号或者密码不能为空");
+        } else if (userBean.getPwd().length() < 4) {
+            return ResultUtil.error(601, "密码不能少于4位");
+        } else if (userBean.getUid().length() < 4) {
+            return ResultUtil.error(601, "账号不能少于4位");
+        } else {
+            userRepository.save(userBean);
+            return ResultUtil.success();
         }
-//        if (userBean.getPwd().length() < 6) {
-//            return ResultUtil.error(601, "密码不能少于6位");
-//        } else if (userBean.getUid().length() < 4) {
-//            return ResultUtil.error(601, "账号不能少于5位");
-//        } else {
-        Map map = new HashMap();
-        userRepository.save(userBean);
-        return ResultUtil.success();
-//        }
     }
 
     public ResultBean login(UserBean userBean) {
         UserBean bean = userRepository.findUserBeanByUid(userBean.getUid());
-        System.out.println("用户名:" + bean.getPwd() + "&" + "密码:" + userBean.getPwd());
-        if (bean.getPwd().equals(userBean.getPwd())) {
+        if (bean == null) {
+            return ResultUtil.error(601, "用户不存在");
+        }
+        if (!bean.getPwd().equals(userBean.getPwd())) {
+            return ResultUtil.error(601, "密码错误");
+        } else {
             Map map = new HashMap();
             map.put("uid", userBean.getUid());
-            return ResultUtil.success(map, "登陆成功");
-        } else {
-            return ResultUtil.error(601, "密码错误或用户不存在");
+            return ResultUtil.success(map);
         }
     }
 }
--- src/main/java/com/yiya/xq/utils/ResultUtil.java
+++ src/main/java/com/yiya/xq/utils/ResultUtil.java
@@ -2,14 +2,19 @@ package com.yiya.xq.utils;
 
 import com.yiya.xq.bean.ResultBean;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class ResultUtil {
 
 
+    public static ResultBean success() {
+        ResultBean result = new ResultBean();
+        result.setCode(200);
+        result.setMsg("OK");
+        result.setData(null);
+        return result;
+    }
+
     //当正确时返回的值
-    public static ResultBean success(Object data){
+    public static ResultBean success(Object data) {
         ResultBean result = new ResultBean();
         result.setCode(200);
         result.setMsg("OK");
@@ -18,7 +23,7 @@ public class ResultUtil {
     }
 
     //当正确时返回的值
-    public static ResultBean success(Object data,String msg){
+    public static ResultBean success(Object data, String msg) {
         ResultBean result = new ResultBean();
         result.setCode(200);
         result.setMsg(msg);
@@ -26,17 +31,12 @@ public class ResultUtil {
         return result;
     }
 
-    public static ResultBean success(){
-        Map map = new HashMap();
-        map.put("state", "success");
-        return success(map);
-    }
-
     //当错误时返回的值
-    public static ResultBean error(int code,String msg){
+    public static ResultBean error(int code, String msg) {
         ResultBean result = new ResultBean();
         result.setCode(code);
         result.setMsg(msg);
+        result.setData(null);
         return result;
     }
 }
